af3b7ed1631244b5b1c3da6e27595a8b9b4ab49a
TadevosyannGarik <tadevosyan12@gmail.com>
2024-03-02 17:32:06 +0400

    Convex Alternate Query

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,36 +1,43 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { api } from "@/convex/_generated/api"
-import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
-import { useMutation, useQuery } from "convex/react"
-import { useEffect } from "react"
+import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { useConvex, useMutation } from 'convex/react'
+import { useEffect } from 'react'
 
-function DashBoard() {
+
+function Dashboard() {
+
+    const convex = useConvex();
     const { user }: any = useKindeBrowserClient();
 
-    const getUser = useQuery(api.user.getUser, {
-        email: user?.email
-    });
-
-    const createUser = useMutation(api.user.createUser)
-
-    useEffect(() => {
+    const createUser = useMutation(api.user.createUser);
+    
+    useEffect( () => { 
         if (user) {
-            if (getUser === undefined) {
-                createUser({
-                    name: user.given_name,
-                    email: user.email,
-                    image: user.picture
-                }).then((resp) => {
-                    console.log(resp)
-                })
-            }
+           checkUser()
         }
-    }, [user])
+    },[user])
+  
+
+    const checkUser = async () => {
+        const result = await convex.query(api.user.getUser, {
+            email: user?.email
+        });
+        if(!result?.length) {
+            createUser({
+                name:user.given_name,
+                email:user.email,
+                image: user.picture || ''
+            }).then((resp)=>{
+                console.log(resp)
+            })
+        }
+    }
 
     return (
-        <div>
+        <div className='p-8'>
             <LogoutLink>
                 <Button>
                     Logout
@@ -40,4 +47,4 @@ function DashBoard() {
     )
 }
 
-export default DashBoard
+export default Dashboard
