6f1014a28ef0f6d4490cc1f323b7bb11ee0ca3ac
TadevosyannGarik <tadevosyan12@gmail.com>
2024-03-02 14:57:30 +0400

    Protect Pages

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,7 +1,17 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs"
 
 function DashBoard() {
     return (
-        <div>page</div>
+        <div>
+            <LogoutLink>
+                <Button>
+                    Logout
+                </Button>
+            </LogoutLink>
+        </div>
     )
 }
 
